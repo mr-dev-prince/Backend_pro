c88f2a42b792a60284e4fb9f364abc97a75dc753
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-01-06 13:40:48 +0530

    Done with uploading

--- conjurring.js
+++ conjurring.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "backendpro";
+export const DB_NAME = "videotube";
--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -14,8 +14,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // file has been uploaded successfully
-    console.log("file uploaded", response.url);
-    return response;
+    fs.unlinkSync(localFilePath)
+     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
