af6f21f5e0a38f6a943a398ff344a6161c6d3b9d
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-01-07 03:55:15 +0530

    ho gya

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -14,8 +14,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // file has been uploaded successfully
-    fs.unlinkSync(localFilePath)
-     return response;
+    fs.unlinkSync(localFilePath);
+    return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
